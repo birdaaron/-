b038fd8ffc57137ded697a7f859f606580a53e7e
birdaaron <735414905@163.com>
2019-07-16 15:41:22 +0800

    初步完成图形界面

--- 06Assembler/src/UI/Controller.java
+++ 06Assembler/src/UI/Controller.java
@@ -1,5 +1,51 @@
 package UI;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+
 public class Controller
 {
+    @FXML
+    public Button btn_file,btn_run,btn_runFast,btn_stop;
+    @FXML
+    private ListView<String> lv_destination,lv_source;
+    @FXML
+    private void initialize()
+    {
+        ObservableList<String> test = FXCollections.observableArrayList("A","B","C");
+        lv_source.setItems(test);
+    }
+    @FXML
+    public void btnFileClick(MouseEvent mouseEvent)
+    {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Open Asm File");
+        fc.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Asm Files","*.asm")
+        );
+        Stage stage = new Stage();
+        File file = fc.showOpenDialog(stage);
+    }
+    @FXML
+    public void btnRunClick(MouseEvent mouseEvent)
+    {
+    }
+    @FXML
+    public void btnRFClick(MouseEvent mouseEvent)
+    {
+    }
+    @FXML
+    public void btnStopClick(MouseEvent mouseEvent)
+    {
+    }
 }
--- 06Assembler/src/UI/Main.java
+++ 06Assembler/src/UI/Main.java
@@ -9,10 +9,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception
+    {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
