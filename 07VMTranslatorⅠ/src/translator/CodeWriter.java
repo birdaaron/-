b71d9f5260f24993cf6d00d18925b4722bb167f9
birdaaron <735414905@163.com>
2019-09-19 22:41:34 +0800

    <07>完成push、pop和add的翻译

--- 07VMTranslatorⅠ/src/translator/CodeWriter.java
+++ 07VMTranslatorⅠ/src/translator/CodeWriter.java
@@ -1,9 +1,89 @@
 package translator;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CodeWriter
 {
     public void setFileName(String fileName){}
-    public void writeArithmetic(String command){}
-    public void WriterPushPop(int commandType,String segment,int index){}
+    public LinkedList<String> writeArithmetic(String command)
+    {
+        LinkedList<String> result = new LinkedList<>();
+        switch (command)
+        {
+            case "add":
+                result.push("@SP");
+                result.push("A=M-1");
+                result.push("D=M");
+                //pop
+                result.push("@SP");
+                result.push("M=M-1");
+                //add
+                result.push("@SP");
+                result.push("A=M-1");
+                result.push("D=M+D");
+                //pop
+                result.push("@SP");
+                result.push("M=M-1");
+                //pushResult
+                result.push("@SP");
+                result.push("A=M");
+                result.push("M=D");
+                result.push("@SP");
+                result.push("M=M+1");
+                break;
+            case "sub":
+                break;
+            case "neg":
+                break;
+            case "eq":
+                break;
+            case "gt":
+                break;
+            case "lt":
+                break;
+            case "and":
+                break;
+            case "or":
+                break;
+            case "not":
+                break;
+        }
+    }
+    public LinkedList<String> writerPushPop(int commandType,String segment,int index)
+    {
+        LinkedList<String> result = new LinkedList<>();
+        String indexString = String.valueOf(index);
+        switch (commandType)
+        {
+            case Parser.C_PUSH:
+                if(segment.equals("constant"))
+                {
+                    result.push("@"+indexString);
+                    result.push("D=A");
+                    result.push("@SP");
+                    result.push("A=M");
+                    result.push("M=D");
+                    result.push("@SP");
+                    result.push("M=M+1");
+                }
+                break;
+            case Parser.C_POP:
+                if(segment.equals("constant"))
+                {
+                    result.push("@SP");
+                    result.push("M=M-1");
+                }
+                break;
+        }
+        return result;
+    }
     public void close() {}
+    public static void main(String args[])
+    {
+        CodeWriter cw = new CodeWriter();
+        LinkedList<String> list = cw.WriterPushPop(Parser.C_PUSH,"constant",7);
+        for(String s : list)
+            System.out.println(s);
+    }
 }
--- 07VMTranslatorⅠ/src/translator/Parser.java
+++ 07VMTranslatorⅠ/src/translator/Parser.java
@@ -2,12 +2,15 @@ package translator;
 
 import java.io.*;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Parser
 {
-    private LinkedList<String> content;
+    private LinkedList<String> content = new LinkedList<>();
     private String currentLine;
-    private final int C_ARITHMETIC = 0,C_PUSH = 1,C_POP = 2,C_LABEL = 3,C_GOTO = 4,
+    public static final int C_ARITHMETIC = 0,C_PUSH = 1,C_POP = 2,C_LABEL = 3,C_GOTO = 4,
                       C_IF = 5,C_FUNCTION = 6,C_RETURN = 7,C_CALL = 8;
     public Parser()
     {
@@ -28,10 +31,86 @@ public class Parser
         }
 
     }
-    public Boolean hasMoreCommands(){return true;}
-    public void advance(){}
-    public int commandType(){return 0;}
-    public String arg1(){return null;}
-    public String arg2(){return null;}
+    public boolean hasMoreCommands()
+    {
+        return (content.size()!=0);
+    }
+    public void advance()
+    {
+        if(hasMoreCommands())
+            currentLine = filterLine(content.poll());
+        else
+            currentLine = null;
+    }
+    public int commandType()
+    {
+        switch(currentLine.substring(0,2))
+        {
+            case "pu":
+                return C_PUSH;
+            case "po":
+                return C_POP;
+            case "la":
+                return C_LABEL;
+            case "go":
+                return C_GOTO;
+            case "if":
+                return C_IF;
+            case "fu":
+                return C_FUNCTION;
+            case "ca":
+                return C_CALL;
+            case "re":
+                return C_RETURN;
+            default:
+                return C_ARITHMETIC;
+        }
 
+    }
+    public String arg1()
+    {
+        switch(commandType())
+        {
+            case C_PUSH:
+                return "push";
+            case C_POP:
+                return "pop";
+            case C_LABEL:
+                return "label";
+            case C_CALL:
+                return "call";
+            case C_FUNCTION:
+                return "function";
+            case C_IF:
+                return "if-goto";
+            case C_GOTO:
+                return "goto";
+            default:
+                return currentLine;
+        }
+    }
+    public String arg2()
+    {
+        String arg2 = filterLine(currentLine.substring(currentLine.indexOf(" ")));
+        int position_space = arg2.indexOf(" ");
+        arg2 = arg2.substring(0,position_space);
+        return arg2;
+    }
+    private String filterLine(String line)
+    {
+        int position_space = 1;
+        int position_note = -1;
+        //过滤空格
+        while(line.substring(0,1).equals(" "))
+            line = line.substring(position_space);
+        //过滤注释
+        position_note = line.indexOf("//");
+        if(position_note!=-1)
+            line = line.substring(0,position_note);
+        return line;
+    }
+    public String getCode()
+    {
+        return null;
+    }
 }
